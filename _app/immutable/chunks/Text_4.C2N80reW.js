f7434f189809fc71837c6c367c967f40679fe427
Giacomo Nanni <gn.nanni@gmail.com>
2024-04-07 22:37:06 +0200

    Updates

--- _app/immutable/chunks/Text_4.C2N80reW.js
+++
@@ -1,3 +0,0 @@
-import{s as i,n as s}from"./scheduler.DGlNlUkI.js";import{S as r,i as h,g as l,h as c,x as d,k as m,a as u,f}from"./index.aSnQmX6g.js";function p(a){let e,n=`<h1>Opinions of the workers</h1> <h2>I asked the workers at Amazon Mechanical Turk to share their honest
-        feelings and opinions on the platform. What do they like and detest? And
-        why do they work on Mturk.</h2>`;return{c(){e=l("section"),e.innerHTML=n,this.h()},l(t){e=c(t,"SECTION",{class:!0,"data-svelte-h":!0}),d(e)!=="svelte-1lp22iu"&&(e.innerHTML=n),this.h()},h(){m(e,"class","desc svelte-i9luj6")},m(t,o){u(t,e,o)},p:s,i:s,o:s,d(t){t&&f(e)}}}class x extends r{constructor(e){super(),h(this,e,null,p,i,{})}}export{x as T};
