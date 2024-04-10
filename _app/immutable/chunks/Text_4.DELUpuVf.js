e26cd2f240ee2ab3ef859a110eb80f6973f7f409
Giacomo Nanni <gn.nanni@gmail.com>
2024-04-10 17:43:10 +0200

    Updates

---
+++ _app/immutable/chunks/Text_4.DELUpuVf.js
@@ -0,0 +1,31 @@
+import{s as i,n as a}from"./scheduler.DlkIgXc2.js";import{S as n,i as h,g as l,h as c,x as u,k as f,a as m,f as d}from"./index.hfIOWo0e.js";function p(o){let e,s=`<h1>Opinions of the workers</h1> <h2>What do you like and detest about working for Amazon Mechanical Turk?
+        Why do you do it? Please share your honest feelings and opinions on the
+        platform.</h2> <p>This time I repeated the task twice, by asking what are the feelings and
+        opinion of the workers on the platform. The first time it was open to
+        all workers on the platform, and the second time only to Mechanical Turk
+        &quot;Masters.&quot;</p> <figure><img src="masters.png" alt="Mechanical Turk" class="svelte-1gua0zx"/> <figcaption>“Mechanical Turk has built technology which analyzes Worker
+            performance, identifies high performing Workers, and monitors their
+            performance over time. Workers who have demonstrated excellence
+            across a wide range of tasks are awarded the Masters Qualification.
+            Masters must continue to pass our statistical monitoring to retain
+            the Mechanical Turk Masters Qualification.”</figcaption></figure> <p>The hiring of Masters involves a different fee charged by Mturks, which
+        is $0.005 compared to $0.040.
+        <figure><img src="fees.png" alt="Mechanical Turk" class="svelte-1gua0zx"/> <figcaption>A screenshot showing the fees of the plaftorm.</figcaption></figure></p> <p>In less than an hour, the first part of the new task was completed. In
+        contrast, after 3 days I closed the task for &quot;masters&quot; only with only 17
+        submissions. The quality of these entries is definitely higher, with
+        responses such as:</p> <quote>“I really liked AMT years ago when I first started working for them.
+        However, over the last year or so, this platform has really gone
+        downhill. The hits are not worth my time. Sometimes they only pay a
+        penny or so for work that is over five minutes long! Also, the rejecters
+        seem to randomly reject work. I recently spent about 8 minutes
+        explaining my response to a chatbot conversation, only to get rejected.
+        I have no idea what they wanted. I have a master&#39;s, so I can get some
+        good hits. However, these unfair rejections are really starting to add
+        up, and I am worried I will lose my master&#39;s status once my rejection
+        rate goes below 99%. What really hurt my rate was a &quot;mass rejection&quot; of
+        10 hits. It turns out that there was a glitch (I read it on
+        MTURKOPTICON), but they would not reverse the rejections. That was so
+        unfair when it was their fault.”.</quote> <p>However, it is clear that there is a lack of such workers. In addition,
+        as already commented with the previous exercise and description above,
+        several workers have complained about the unexplained rejections that
+        often occur in the platform.</p>`;return{c(){e=l("section"),e.innerHTML=s,this.h()},l(t){e=c(t,"SECTION",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-1r0oits"&&(e.innerHTML=s),this.h()},h(){f(e,"class","desc svelte-1gua0zx")},m(t,r){m(t,e,r)},p:a,i:a,o:a,d(t){t&&d(e)}}}class y extends n{constructor(e){super(),h(this,e,null,p,i,{})}}export{y as T};
