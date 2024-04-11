3bbd7d7a3f9fc8c3b328b077623a49a28a4edec1
Giacomo Nanni <gn.nanni@gmail.com>
2024-04-11 10:06:31 +0200

    Updates

--- _app/immutable/nodes/2.BoomsBOe.js
+++ _app/immutable/nodes/2.BoomsBOe.js
@@ -1,9 +1,9 @@
-import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,g as l,h,x as K,k as o,a as G,f as n,r as T,s as z,j as k,u as x,c as E,l as C,y as i,v as _,d as q,t as A,w as M}from"../chunks/index.hfIOWo0e.js";import{T as U}from"../chunks/Text_1.BfSo3o2i.js";import{T as X}from"../chunks/Text_2.CVivQ-O7.js";import{T as Q}from"../chunks/Text_3.CzR_Xc0o.js";import{T as Y}from"../chunks/Text_4.DELUpuVf.js";function Z(I){let e,r=`<h1>Who is working for Amazon Mechanical Turk?</h1> <h2>These experiments delve into the lives of Amazon Mechanical Turk
+import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as F,i as R,g as l,h,x as K,k as o,a as V,f as n,r as T,s as E,j as b,u as x,c as z,l as S,y as i,v as _,d as q,t as A,w as M}from"../chunks/index.hfIOWo0e.js";import{T as U}from"../chunks/Text_1.BfSo3o2i.js";import{T as Q}from"../chunks/Text_2.CVivQ-O7.js";import{T as X}from"../chunks/Text_3.CzR_Xc0o.js";import{T as Y}from"../chunks/Text_4.DELUpuVf.js";function Z(I){let e,r=`<h1>Who is working for Amazon Mechanical Turk?</h1> <h2>These experiments delve into the lives of Amazon Mechanical Turk
         workers. Following the logic of the platform, workers have been employed
         for a small fee and asked to give a glimpse of their private lifes.
         Unlike the typical use of the platform, the submitted data prompt
         reflection on hidden labor, material quality, and ways of rebelling
-        against a system on the verge of ethics.</h2> <figure><img src="./mturk.png" alt="Mechanical Turk" class="svelte-1tunlep"/> <figcaption>Drawing of von Kempelen&#39;s Mechanical Turk (Villatoro 2013).</figcaption></figure> <p>The ever-growing use of platforms such as Amazon Mechanical Turk, allow
+        against a system on the verge of ethics.</h2> <figure><img src="./mturk.png" alt="Mechanical Turk" class="svelte-o1xcl"/> <figcaption>Drawing of von Kempelen&#39;s Mechanical Turk (Villatoro 2013).</figcaption></figure> <p>The ever-growing use of platforms such as Amazon Mechanical Turk, allow
         large groups of individuals to participate in creating, tagging and
         describing data.
         <br/>
@@ -11,7 +11,8 @@ import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,
         “automatic” chess-playing machine, which was handily beating humans in chess
         games. Of course, the robot was not using any artificial intelligence algorithms
         back then. The secret of the Mechanical Turk machine was a human operator,
-        hidden inside the machine, who was the real intelligence (Ipeirotis, 2010).
+        hidden inside the machine,who was the real intelligence.
+        <a href="https://doi.org/10.1145/1869086.1869094" target="_blank" class="svelte-o1xcl">See link</a>.
         <br/><br/>
 
         Today, Amazon Mechanical Turk is a marketplace for tasks that cannot be
@@ -24,7 +25,7 @@ import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,
         counterexamples.
         <br/>
         Such data is analysed and processed by workers on platforms like Mechanical
-        Turks. But little is known about these individuals that complete these tasks.</p> <figure><img src="./interface.png" alt="Mechanical Turk" class="svelte-1tunlep"/> <figcaption>Main page of the platform.</figcaption></figure> <quote>“The view is partially blocked by a large hedge/bush of some sort. There
+        Turks. But little is known about these individuals that complete these tasks.</p> <figure><img src="./interface.png" alt="Mechanical Turk" class="svelte-o1xcl"/> <figcaption>Main page of the platform.</figcaption></figure> <quote>“The view is partially blocked by a large hedge/bush of some sort. There
         are large deciduous trees off in the distance, and what appears to be an
         overcast sky. In the far right window pane there is a flowering bush
         with pink flowers.”</quote> <p>On July 02, 2022 I uploaded 200 images on Amazon Mechanical Turkand I
@@ -39,8 +40,8 @@ import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,
         glimpse of their private life, their view close to their desk, their
         working station. At the limit of ethics and labour exploitation I asked
         myself whether this is an acceptable approach to use in research and in
-        general, if employing random workers around the world is the
-        ideal practice to shape future artificial intelligences.</p> <p>While both tasks were performed, the material collected varied
+        general, if employing random workers around the world is the ideal
+        practice to shape future artificial intelligences.</p> <p>While both tasks were performed, the material collected varied
         considerably: from blurred photos to details of curtains, from
         landscapes to computer screenshots.</p> <p>The same consideration can be made with the texts collected. Some
         descriptions are lengthy, others are short, some are tags, others
@@ -58,7 +59,7 @@ import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,
         entirety on this site.</p> <quote>“Information has always had geography. It is from somewhere; about
         somewhere; it evolves and is transformed somewhere; it is mediaxted by
         networks, infrastructures, and technologies: all of which exist in
-        physical, material places” (Graham et al., 2015).</quote> <p>Datasets are no exception. The information they carry is a reflection of
+        physical, material places”. <a href="https://rgs-ibg.onlinelibrary.wiley.com/doi/full/10.1002/geo2.8" target="_blank" class="svelte-o1xcl">See link</a>.</quote> <p>Datasets are no exception. The information they carry is a reflection of
         time, politics and geographical contexts.
         <br/>
         Although, in the past years, there has been a growing attention of bias studies
@@ -67,11 +68,5 @@ import{s as N,n as $}from"../chunks/scheduler.DlkIgXc2.js";import{S as R,i as F,
         a central focus on the geographies of Amazon Mechanical Turk. Not necessarly
         on the demographics of its workers, but rather on the different places where
         data is annotated and created.
-        <br/></p> <h3>Bibliographic reference</h3> <p>Ipeirotis, P. G. (2010). Analyzing the Amazon Mechanical Turk
-        marketplace. XRDS: Crossroads, The ACM Magazine for Students, 17(2),
-        16–21. https://doi.org/10.1145/1869086.1869094
-        <br/><br/>
-        Graham et al., 2015, Towards a study of information geographies: [im]mutable
-        augmentations and a mapping of the geographies of information. Retrieved
-        17 January 2019, from https://rgs-ibg.onlinelibrary.wiley.com/doi/full/10.1002/geo2.8.</p>`;return{c(){e=l("section"),e.innerHTML=r,this.h()},l(t){e=h(t,"SECTION",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-71kvqa"&&(e.innerHTML=r),this.h()},h(){o(e,"class","desc svelte-1tunlep")},m(t,u){G(t,e,u)},p:$,i:$,o:$,d(t){t&&n(e)}}}class ee extends R{constructor(e){super(),F(this,e,null,Z,N,{})}}function te(I){let e,r=`<h1>Trying to work for Amazon Mechanical Turk</h1> <h2>I am desperately trying to create a worker account to earn back what I
-        have spent on the platform.</h2> <figure><img src="./suspended.png" alt="Mechanical Turk" class="svelte-9q0th3"/> <figcaption>A screenshot of the page denying me access.</figcaption></figure>`;return{c(){e=l("section"),e.innerHTML=r,this.h()},l(t){e=h(t,"SECTION",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-ahvf63"&&(e.innerHTML=r),this.h()},h(){o(e,"class","desc svelte-9q0th3")},m(t,u){G(t,e,u)},p:$,i:$,o:$,d(t){t&&n(e)}}}class ae extends R{constructor(e){super(),F(this,e,null,te,N,{})}}function se(I){let e,r,t,u,m,p,O,c,g,D,d,w,H,f,y,L,v,b,S;return t=new ee({}),p=new U({}),g=new X({}),w=new Q({}),y=new Y({}),b=new ae({}),{c(){e=l("article"),r=l("div"),T(t.$$.fragment),u=z(),m=l("a"),T(p.$$.fragment),O=z(),c=l("a"),T(g.$$.fragment),D=z(),d=l("a"),T(w.$$.fragment),H=z(),f=l("a"),T(y.$$.fragment),L=z(),v=l("div"),T(b.$$.fragment),this.h()},l(a){e=h(a,"ARTICLE",{class:!0});var s=k(e);r=h(s,"DIV",{style:!0,class:!0});var J=k(r);x(t.$$.fragment,J),J.forEach(n),u=E(s),m=h(s,"A",{href:!0,class:!0});var P=k(m);x(p.$$.fragment,P),P.forEach(n),O=E(s),c=h(s,"A",{href:!0,style:!0,class:!0});var V=k(c);x(g.$$.fragment,V),V.forEach(n),D=E(s),d=h(s,"A",{href:!0,style:!0,class:!0});var j=k(d);x(w.$$.fragment,j),j.forEach(n),H=E(s),f=h(s,"A",{href:!0,style:!0,class:!0});var B=k(f);x(y.$$.fragment,B),B.forEach(n),L=E(s),v=h(s,"DIV",{style:!0,class:!0});var W=k(v);x(b.$$.fragment,W),W.forEach(n),s.forEach(n),this.h()},h(){C(r,"--color-1","#8d9a8f"),o(r,"class","svelte-4mwio5"),o(m,"href","views"),o(m,"class","svelte-4mwio5"),o(c,"href","exausting"),C(c,"--color-1","#ffe0e0"),o(c,"class","svelte-4mwio5"),o(d,"href","screens"),C(d,"--color-1","#acb2c8"),o(d,"class","svelte-4mwio5"),o(f,"href","opinions"),C(f,"--color-1","#aec8ac"),o(f,"class","svelte-4mwio5"),C(v,"--color-1","#c2c2c2"),o(v,"class","svelte-4mwio5"),o(e,"class","container svelte-4mwio5")},m(a,s){G(a,e,s),i(e,r),_(t,r,null),i(e,u),i(e,m),_(p,m,null),i(e,O),i(e,c),_(g,c,null),i(e,D),i(e,d),_(w,d,null),i(e,H),i(e,f),_(y,f,null),i(e,L),i(e,v),_(b,v,null),S=!0},p:$,i(a){S||(q(t.$$.fragment,a),q(p.$$.fragment,a),q(g.$$.fragment,a),q(w.$$.fragment,a),q(y.$$.fragment,a),q(b.$$.fragment,a),S=!0)},o(a){A(t.$$.fragment,a),A(p.$$.fragment,a),A(g.$$.fragment,a),A(w.$$.fragment,a),A(y.$$.fragment,a),A(b.$$.fragment,a),S=!1},d(a){a&&n(e),M(t),M(p),M(g),M(w),M(y),M(b)}}}class ce extends R{constructor(e){super(),F(this,e,null,se,N,{})}}export{ce as component};
+        <br/></p>`;return{c(){e=l("section"),e.innerHTML=r,this.h()},l(t){e=h(t,"SECTION",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-rkzwm4"&&(e.innerHTML=r),this.h()},h(){o(e,"class","desc svelte-o1xcl")},m(t,u){V(t,e,u)},p:$,i:$,o:$,d(t){t&&n(e)}}}class ee extends F{constructor(e){super(),R(this,e,null,Z,N,{})}}function te(I){let e,r=`<h1>Trying to work for Amazon Mechanical Turk</h1> <h2>I am desperately trying to create a worker account to earn back what I
+        have spent on the platform.</h2> <figure><img src="./suspended.png" alt="Mechanical Turk" class="svelte-9q0th3"/> <figcaption>A screenshot of the page denying me access.</figcaption></figure>`;return{c(){e=l("section"),e.innerHTML=r,this.h()},l(t){e=h(t,"SECTION",{class:!0,"data-svelte-h":!0}),K(e)!=="svelte-ahvf63"&&(e.innerHTML=r),this.h()},h(){o(e,"class","desc svelte-9q0th3")},m(t,u){V(t,e,u)},p:$,i:$,o:$,d(t){t&&n(e)}}}class ae extends F{constructor(e){super(),R(this,e,null,te,N,{})}}function se(I){let e,r,t,u,m,p,O,c,g,H,d,w,D,f,y,L,v,k,C;return t=new ee({}),p=new U({}),g=new Q({}),w=new X({}),y=new Y({}),k=new ae({}),{c(){e=l("article"),r=l("div"),T(t.$$.fragment),u=E(),m=l("a"),T(p.$$.fragment),O=E(),c=l("a"),T(g.$$.fragment),H=E(),d=l("a"),T(w.$$.fragment),D=E(),f=l("a"),T(y.$$.fragment),L=E(),v=l("div"),T(k.$$.fragment),this.h()},l(a){e=h(a,"ARTICLE",{class:!0});var s=b(e);r=h(s,"DIV",{style:!0,class:!0});var j=b(r);x(t.$$.fragment,j),j.forEach(n),u=z(s),m=h(s,"A",{href:!0,class:!0});var J=b(m);x(p.$$.fragment,J),J.forEach(n),O=z(s),c=h(s,"A",{href:!0,style:!0,class:!0});var P=b(c);x(g.$$.fragment,P),P.forEach(n),H=z(s),d=h(s,"A",{href:!0,style:!0,class:!0});var W=b(d);x(w.$$.fragment,W),W.forEach(n),D=z(s),f=h(s,"A",{href:!0,style:!0,class:!0});var B=b(f);x(y.$$.fragment,B),B.forEach(n),L=z(s),v=h(s,"DIV",{style:!0,class:!0});var G=b(v);x(k.$$.fragment,G),G.forEach(n),s.forEach(n),this.h()},h(){S(r,"--color-1","#8d9a8f"),o(r,"class","svelte-4mwio5"),o(m,"href","views"),o(m,"class","svelte-4mwio5"),o(c,"href","exausting"),S(c,"--color-1","#ffe0e0"),o(c,"class","svelte-4mwio5"),o(d,"href","screens"),S(d,"--color-1","#acb2c8"),o(d,"class","svelte-4mwio5"),o(f,"href","opinions"),S(f,"--color-1","#aec8ac"),o(f,"class","svelte-4mwio5"),S(v,"--color-1","#c2c2c2"),o(v,"class","svelte-4mwio5"),o(e,"class","container svelte-4mwio5")},m(a,s){V(a,e,s),i(e,r),_(t,r,null),i(e,u),i(e,m),_(p,m,null),i(e,O),i(e,c),_(g,c,null),i(e,H),i(e,d),_(w,d,null),i(e,D),i(e,f),_(y,f,null),i(e,L),i(e,v),_(k,v,null),C=!0},p:$,i(a){C||(q(t.$$.fragment,a),q(p.$$.fragment,a),q(g.$$.fragment,a),q(w.$$.fragment,a),q(y.$$.fragment,a),q(k.$$.fragment,a),C=!0)},o(a){A(t.$$.fragment,a),A(p.$$.fragment,a),A(g.$$.fragment,a),A(w.$$.fragment,a),A(y.$$.fragment,a),A(k.$$.fragment,a),C=!1},d(a){a&&n(e),M(t),M(p),M(g),M(w),M(y),M(k)}}}class ce extends F{constructor(e){super(),R(this,e,null,se,N,{})}}export{ce as component};
