c98f7fa288ccb43adb415bed980e5cf1591091ac
Giacomo Nanni <gn.nanni@gmail.com>
2024-04-11 11:05:16 +0200

    Updates

---
+++ _app/immutable/chunks/Text_4.v4i24VyE.js
@@ -0,0 +1,27 @@
+import{s as i,n as a}from"./scheduler.DlkIgXc2.js";import{S as r,i as h,g as l,h as c,x as u,k as f,a as d,f as p}from"./index.hfIOWo0e.js";function m(s){let e,o=`<h1>Opinions of the workers</h1> <h2>What do you like and detest about working for Amazon Mechanical Turk?
+        Why do you do it? Please share your honest feelings and opinions on the
+        platform.</h2> <p>This time I repeated the task twice, by asking what are the feelings and
+        opinion of the workers on the platform. The first time it was open to
+        all workers on the platform, and the second time only to Mechanical Turk
+        &quot;Masters.&quot;</p> <figure><img src="masters.png" alt="Mechanical Turk" class="svelte-1gua0zx"/> <figcaption>“Mechanical Turk has built technology which analyzes Worker
+            performance, identifies high performing Workers, and monitors their
+            performance over time. Workers who have demonstrated excellence
+            across a wide range of tasks are awarded the Masters Qualification.
+            Masters must continue to pass our statistical monitoring to retain
+            the Mechanical Turk Masters Qualification.”</figcaption></figure> <p>The hiring of Masters involves a different fee charged by Mturks, which
+        is $0.005 compared to $0.040.
+        <figure><img src="fees.png" alt="Mechanical Turk" class="svelte-1gua0zx"/> <figcaption>A screenshot showing the fees of the plaftorm.</figcaption></figure></p> <p>In less than an hour, the first part of the new task was completed. In
+        contrast, after 3 days I closed the task for &quot;masters&quot; only with only 17
+        submissions. The quality of these entries is definitely higher, with
+        responses such as:</p> <quote>“On one hand, mturk changed my life. I have C-PTSD and find public
+        places too noisy and stressful. I was on the verge of becoming
+        completely homeless but went back to working on here (I previously
+        worked a lot on here when in college but then took a break). It now
+        gives me a chance to earn enough money to survive. On the other hand,
+        it&#39;s a terrible way to make a living. It&#39;s not stable and can be
+        stressful. I have found that working on other platforms and bouncing
+        back and forth between them helps so there isn&#39;t as much down time
+        waiting for work”.</quote> <p>However, it is clear that there is a lack of such workers. In addition,
+        as already commented with the previous exercise and description above,
+        several workers have complained about the unexplained rejections that
+        often occur in the platform.</p>`;return{c(){e=l("section"),e.innerHTML=o,this.h()},l(t){e=c(t,"SECTION",{class:!0,"data-svelte-h":!0}),u(e)!=="svelte-17zkj9p"&&(e.innerHTML=o),this.h()},h(){f(e,"class","desc svelte-1gua0zx")},m(t,n){d(t,e,n)},p:a,i:a,o:a,d(t){t&&p(e)}}}class w extends r{constructor(e){super(),h(this,e,null,m,i,{})}}export{w as T};
